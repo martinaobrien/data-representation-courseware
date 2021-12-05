106a92f9e47e80420616b480b95ee831b6d74e98
martinaobrien <G00376463@gmit.ie>
2021-12-05 09:04:10 +0000

    Create insertinto.py
    updating a database to connect to server from Python

---
+++ insertinto.py
@@ -0,0 +1,17 @@
+import mysql.connector
+# This Database is created when you add the name to the last word in cursor execute
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="martinadatarepresentation"
+
+)
+cursor = db.cursor()
+sql="insert into student2(name, age) values (%s,%s)"
+values = ("Mary", 20)
+
+cursor.execute(sql, values)
+
+db.commit()
+print("1 record inserted, ID:", cursor.lastrowid)
