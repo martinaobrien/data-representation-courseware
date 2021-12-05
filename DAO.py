dbb506df2b7c86c763aacbc1d01c8dff4f442c2e
martinaobrien <G00376463@gmit.ie>
2021-12-05 09:01:32 +0000

    Create DAO.py
    DAO for project

---
+++ DAO.py
@@ -0,0 +1,21 @@
+from studentsDAO import studentDAO
+
+# create
+
+latestid = studentDAO.create(("mark", 45))
+
+result = studentDAO.findByID(latestid);
+print(result)
+
+studentDAO.update(("Fred", 21, latestid))
+result = studentDAO.findByID(latestid)
+print(result)
+
+# get all
+allStudents = studentDAO.getAll()
+for student in allStudents:
+    print(student)
+
+# delete
+
+studentDAO.delete(latestid)
