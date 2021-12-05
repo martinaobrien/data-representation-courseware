9f6be5ff301fd9a89b751dbfcf9a3b5c52a7bf86
martinaobrien <G00376463@gmit.ie>
2021-12-05 11:28:29 +0000

    Update DAO.py
    Trying to degub DAO

--- DAO.py
+++ DAO.py
@@ -2,19 +2,19 @@ from studentsDAO import studentDAO
 
 # create
 
-latestid = studentDAO.create(("mark", 45))
+latestid = studentDAO.create(("JUNE", 18))
 
 result = studentDAO.findByID(latestid);
 print(result)
 
-studentDAO.update(("Fred", 21, latestid))
-result = studentDAO.findByID(latestid)
-print(result)
+#studentDAO.update(("Fred", 21, latestid))
+#result = studentDAO.findByID(latestid)
+#print(result)
 
 # get all
-allStudents = studentDAO.getAll()
-for student in allStudents:
-    print(student)
+#allStudents = studentDAO.getAll()
+#for student in allStudents:
+    #print(student)
 
 # delete
 
